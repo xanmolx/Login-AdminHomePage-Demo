e60c92755eb8cfee7135908c25fa65afc58fbf6a
Anmol Singh Sethi <anmolsinghsethi@yahoo.com>
2017-07-03 19:51:30 +0530

    Create Dept.java

---
+++ src/studentcompanion/Dept.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentcompanion;
+
+/**
+ *
+ * @author anmol singh sethi
+ */
+public class Dept {
+    String department;
+    String description;
+
+    public Dept(String department, String description) {
+        this.department = department;
+        this.description = description;
+    }
+    
+    
+    
+    
+}
+
